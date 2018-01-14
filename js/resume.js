f21ad116b20365cff84d44213d9cb58120129e9a
cassiecardiff <34712766+cassiecardiff@users.noreply.github.com>
2018-01-14 12:59:21 +0000

    Update resume.js

--- js/resume.js
+++ js/resume.js
@@ -26,3 +26,12 @@
   });
 
 })(jQuery); // End of use strict
+
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: true,
+    interval: 4000,
+  });
+});
